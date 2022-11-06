43802e2cac8b9b86a4d5a2773ec649612c1c00e1
Mila <milaivanova2609@gmail.com>
2022-11-06 23:11:00 +0200

    Completed!

--- Pages/Contact.xaml.cs
+++ Pages/Contact.xaml.cs
@@ -23,6 +23,18 @@ namespace WPF
         public Contact()
         {
             InitializeComponent();
+
+            Style style = new Style(this);
+            CloseButton.Click += style.Close_Click;
+            MinimizeButton.Click += style.Minimize_Click;
+            MaximizeButton.Click += style.Maximize_Click;
+
+            BorderNavigator borderNavigator = new BorderNavigator();
+            menu.Click += borderNavigator.MenuButton_Click;
+            restaurant.Click += borderNavigator.RestaurantButton_Click;
+            classes.Click += borderNavigator.ClassesButton_Click;
+            booking.Click += borderNavigator.BookButton_Click;
         }
+        private void MainPage_Click(object sender, RoutedEventArgs e) => Switcher.Switch(new MainPage());
     }
 }
